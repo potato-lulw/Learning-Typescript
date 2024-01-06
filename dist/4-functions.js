208fbb22cec81ca3d3d9c0a889e4e6f290f866e4
potato-lulw <om.patil17072@gmail.com>
2024-01-07 00:49:26 +0530

    ⚙ Function and return type

---
+++ dist/4-functions.js
@@ -0,0 +1,28 @@
+"use strict";
+//! functions
+// we have to explicitly mention the types of the variables to avoid accepting wrong parameters
+// function addTwoNumber(a: number, b: number){
+//     return a + b;
+// }
+// we can also mention the return type using a colen after the variable declaration
+function addTwoNumber(a, b) {
+    return a + b;
+}
+// using arrow functions
+const subtractTwoNumber = (a, b) => {
+    return a - b;
+};
+console.log(addTwoNumber(1, 2));
+console.log(subtractTwoNumber(1, 2));
+// function with an array 
+// if there was no return type, it returns void 
+const addAllNumbers = (items) => {
+    const sum = items.reduce((a, b) => a + b, 0);
+    return sum;
+};
+console.log(addAllNumbers([1, 2, 3, 4, 5]));
+// return type inference
+const formatGreeting = (name, greeting) => {
+    return `${greeting}, ${name}`;
+};
+console.log(formatGreeting('om', "Hello"));
