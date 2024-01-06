6be828b7efc898b479031d136bbf5187309e1379
potato-lulw <om.patil17072@gmail.com>
2024-01-07 01:23:33 +0530

    🥴Any type in TS

---
+++ dist/5-any.js
@@ -0,0 +1,16 @@
+"use strict";
+//! Any type
+// basically reverting back to JS where any variable can have any datatype
+let age;
+age = 30;
+age = false;
+// no errors
+// Any type with arrays
+let lists = ['wow', 2, true];
+// Function with any type
+const addTogether = (value) => {
+    return value + value;
+};
+console.log(addTogether('hello'));
+console.log(addTogether(1));
+console.log(addTogether('1' + 1));
